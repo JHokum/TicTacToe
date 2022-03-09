8847fa3a75fcc67f1ba15bf69413a6c864080ed3
JHokum <jdelgado92@hotmail.com>
2022-03-09 13:41:01 -0800

    Display Module

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 
 //Gameboard module
 const Gameboard = (function(){
-    const gameboard = [];
-    const meow = ()=>console.log("meow");
-    return {gameboard,meow}
+    const gameboard = ['X','O','O','O','X','X','X','X','O'];
+    
+    return {gameboard}
 })();
+
+const Display = (function(){
+    const displayBoard = function(gameboard){
+        const body = document.querySelector("body");
+        console.log(gameboard);
+        for (let marker of gameboard){
+            let mark = document.createElement("span");
+            mark.textContent=marker;
+            console.log(`marker ${marker}`)
+            body.appendChild(mark);
+        }
+    }
+    return{displayBoard};
+    
+})()
 
 //Player factory
 const Player = function(select){
     const name = "";
     const marker="";
-
+    
+    //Will return Human or computer
+    
     switch(select){
         case 1:
             name="Human";
@@ -24,5 +41,8 @@ const Player = function(select){
             return {};
     }
 }
+//Human player object will inherit Player
 
-Gameboard.meow();
+//Computer player object will inherit Player
+
+Display.displayBoard(Gameboard.gameboard);
